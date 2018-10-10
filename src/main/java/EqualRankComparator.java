e9008c3896cfadb42109f740c765f51d8f289771
gladysleng <gladys_yinyuen@hotmail.com>
2018-10-10 05:36:19 -0400

    created compareRoyalFlush methods

--- src/main/java/EqualRankComparator.java
+++ src/main/java/EqualRankComparator.java
@@ -1,2 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+
 public class EqualRankComparator {
+    private HandChecker handChecker = new HandChecker();
+
+    public List<Card> compareRoyalFlush(List<Card> c1,List<Card>c2){
+        if(c1.get(0).getSuit() > c2.get(0).getSuit()){
+            return c1;
+        }
+        else{
+            return c2;
+        }
+    }
 }
