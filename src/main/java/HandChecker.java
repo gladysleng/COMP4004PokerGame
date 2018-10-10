928fc6d6d7b7eb66b2b65bc0296ed846313a42a8
gladysleng <gladys_yinyuen@hotmail.com>
2018-10-10 04:27:38 -0400

    created sortHand and Suit function for the previous test commit

--- src/main/java/HandChecker.java
+++ src/main/java/HandChecker.java
@@ -1,5 +1,9 @@
+
 import java.util.List;
-import java.util.ArrayList;
+
+import java.util.Collections;
+import java.util.Comparator;
+
 public class HandChecker {
 
     private int TWO_OF_A_KIND = 2;
@@ -11,6 +15,32 @@ public class HandChecker {
         return c.size() == 5;
     }
 
+    // sort the hand in ascending order according to rank
+    public void sortHand(List<Card> c) {
+        Collections.sort(c, new Comparator<Card>() {
+            public int compare(Card c1, Card c2) {
 
+                int result = c1.getRank() - c2.getRank();
+
+                if (result != 0) {
+                    return result;
+
+                } else { // if their rank are equal, compare the suit
+                    int result2 = c1.getSuit() - c2.getSuit();
+                    return result2;
+                }
+
+            }
+        });
+    }
+
+    // sort suit according to suit rank
+    public void sortSuit(List<Card> c) {
+        Collections.sort(c, new Comparator<Card>() {
+            public int compare(Card card1, Card card2) {
+                return card1.getSuit() - card2.getSuit();
+            }
+        });
+    }
 
 }
