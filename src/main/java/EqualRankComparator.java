4158bda95df8379b45af48957c02fb48085048d8
gladysleng <gladys_yinyuen@hotmail.com>
2018-10-10 05:46:18 -0400

    compare highcard method

--- src/main/java/EqualRankComparator.java
+++ src/main/java/EqualRankComparator.java
@@ -6,37 +6,34 @@ import java.util.Map;
 public class EqualRankComparator {
     private HandChecker handChecker = new HandChecker();
 
-    public List<Card> compareRoyalFlush(List<Card> c1,List<Card>c2){
-        if(c1.get(0).getSuit() > c2.get(0).getSuit()){
+    public List<Card> compareRoyalFlush(List<Card> c1, List<Card> c2) {
+        if (c1.get(0).getSuit() > c2.get(0).getSuit()) {
             return c1;
-        }
-        else{
+        } else {
             return c2;
         }
     }
-    public List<Card> compareStraightFlush(List<Card>c1, List<Card>c2){
+
+    public List<Card> compareStraightFlush(List<Card> c1, List<Card> c2) {
         handChecker.sortHand(c1);
         handChecker.sortHand(c2);
 
-        if(c1.get(c1.size()-1).getRank() == c2.get(c2.size()-1).getRank()){
-            if(c1.get(c1.size()-1).getSuit() > c2.get(c2.size() - 1).getSuit()){
+        if (c1.get(c1.size() - 1).getRank() == c2.get(c2.size() - 1).getRank()) {
+            if (c1.get(c1.size() - 1).getSuit() > c2.get(c2.size() - 1).getSuit()) {
                 return c1;
-            }
-            else{
+            } else {
                 return c2;
             }
-        }
-        else{
-            if(c1.get(c1.size()-1).getRank() > c2.get(c2.size()-1).getRank()){
+        } else {
+            if (c1.get(c1.size() - 1).getRank() > c2.get(c2.size() - 1).getRank()) {
                 return c1;
-            }
-            else{
+            } else {
                 return c2;
             }
         }
     }
 
-    private List<Card> compareCenterCard(List<Card>c1,List<Card>c2){
+    private List<Card> compareCenterCard(List<Card> c1, List<Card> c2) {
         handChecker.sortHand(c1);
         handChecker.sortHand(c1);
 
@@ -47,12 +44,13 @@ public class EqualRankComparator {
         }
     }
 
-    public List<Card> compareFourOfAKind(List<Card>c1, List<Card>c2){
-        return compareCenterCard(c1,c2);
+    public List<Card> compareFourOfAKind(List<Card> c1, List<Card> c2) {
+        return compareCenterCard(c1, c2);
 
     }
+
     public List<Card> compareFullHouse(List<Card> c1, List<Card> c2) {
-        return compareCenterCard(c1,c2);
+        return compareCenterCard(c1, c2);
 
     }
 
@@ -61,6 +59,7 @@ public class EqualRankComparator {
         return compareCenterCard(c1, c2);
 
     }
+
     public List<Card> compareFlush(List<Card> c1, List<Card> c2) {
         handChecker.sortHand(c1);
         handChecker.sortHand(c2);
@@ -84,28 +83,26 @@ public class EqualRankComparator {
 
     }
 
-    private List<Card> compareHighestRankOfTwoHands(List<Card>c1 ,List<Card> c2){
+    private List<Card> compareHighestRankOfTwoHands(List<Card> c1, List<Card> c2) {
         handChecker.sortHand(c1);
         handChecker.sortHand(c2);
 
-        if(c1.get(c1.size()-1).getRank() > c2.get(c2.size()-1).getRank()){
+        if (c1.get(c1.size() - 1).getRank() > c2.get(c2.size() - 1).getRank()) {
             return c1;
-        }
-        else if (c2.get(c2.size()-1).getRank() > c1.get(c1.size()-1).getRank()){
+        } else if (c2.get(c2.size() - 1).getRank() > c1.get(c1.size() - 1).getRank()) {
+            return c2;
+        } else if (c1.get(c1.size() - 1).getSuit() > c2.get(c2.size() - 1).getSuit()) {
+            return c1;
+        } else {
             return c2;
         }
-        else if (c1.get(c1.size()-1).getSuit() > c2.get(c2.size()-1).getSuit()){
-            return c1;
-        }
-        else{
-            return c2;
-        }
-    }
-    public List<Card> compareStraight(List<Card>c1 ,List<Card> c2){
-        return compareHighestRankOfTwoHands(c1,c2);
     }
 
-    private Card returnHighestCardOfPairs(List<Card>c){
+    public List<Card> compareStraight(List<Card> c1, List<Card> c2) {
+        return compareHighestRankOfTwoHands(c1, c2);
+    }
+
+    private Card returnHighestCardOfPairs(List<Card> c) {
         HashMap<Integer, List<Card>> rankBucket = new HashMap<Integer, List<Card>>();
         List<Card> cards;
         for (Card currentCard : c) {
@@ -126,33 +123,36 @@ public class EqualRankComparator {
 
         List<Card> highestPair = highestRankMap.getValue();
         handChecker.sortSuit(highestPair);
-        return highestPair.get(highestPair.size()-1);
+        return highestPair.get(highestPair.size() - 1);
     }
 
-    private List<Card> compareHighestPair(List<Card>c1 ,List<Card> c2){
+    private List<Card> compareHighestPair(List<Card> c1, List<Card> c2) {
         Card highestFromC1 = returnHighestCardOfPairs(c1);
         Card highestFromC2 = returnHighestCardOfPairs(c2);
 
-        if(highestFromC1.getRank() > highestFromC2.getRank()){
+        if (highestFromC1.getRank() > highestFromC2.getRank()) {
             return c1;
-        }
-        else if (highestFromC2.getRank() > highestFromC1.getRank()){
+        } else if (highestFromC2.getRank() > highestFromC1.getRank()) {
             return c2;
-        }
-        else if (highestFromC1.getSuit() > highestFromC2.getSuit()){
+        } else if (highestFromC1.getSuit() > highestFromC2.getSuit()) {
             return c1;
-        }
-        else{
+        } else {
             return c2;
         }
 
     }
-    public List<Card> compareTwoPairs(List<Card>c1 ,List<Card> c2){
-        return compareHighestPair(c1,c2);
+
+    public List<Card> compareTwoPairs(List<Card> c1, List<Card> c2) {
+        return compareHighestPair(c1, c2);
     }
 
-    public List<Card> compareOnePair(List<Card>c1 ,List<Card> c2){
-        return compareHighestPair(c1,c2);
+    public List<Card> compareOnePair(List<Card> c1, List<Card> c2) {
+        return compareHighestPair(c1, c2);
+
+    }
+
+    public List<Card> compareHighCard(List<Card> c1, List<Card> c2) {
+        return compareHighestRankOfTwoHands(c1, c2);
 
     }
 }
